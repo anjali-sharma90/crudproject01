f6028289490ff360014869b637d3adee0601f817
anjali-sharma911 <anjali.11587@gmail.com>
2024-11-13 13:56:40 +0530

    intial commit

---
+++ src/EmployeeData.js
@@ -0,0 +1,22 @@
+export const EmployeeData = [
+  {
+    id: 1,
+    firstName: "Mark",
+    lastName: "David",
+    age: 20,
+  },
+
+  {
+    id: 2,
+    firstName: "Virat",
+    lastName: "Kohli",
+    age: 33,
+  },
+
+  {
+    id: 3,
+    firstName: "Rohit",
+    lastName: "Sharma",
+    age: 35,
+  },
+];
